d2f1c8e184a19dc49bfceb65c79b1635265dc1c9
Eatmyself <nagin2358@gmail.com>
2020-11-23 16:51:46 +0800

    Initial Commit

---
+++ card/cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardApp.swift
+//  card
+//
+//  Created by Eatmyself on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
